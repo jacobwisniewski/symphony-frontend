c02386fbde8b90bbbc7a73c7f4423d3a8ee7a480
jacobwisniewski <jacob.wisniewski.m@gmail.com>
2018-09-06 19:39:57 +1000

    Added root reducer file

---
+++ client/src/modules/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+// This is where we can import reducers from this folder
+
+export default combineReducers({
+    // Where we can stick our reducers in to create a root reducer
+})
