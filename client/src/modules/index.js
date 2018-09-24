e07a9aba9fa504cfac4a507404fa009f1bb997ee
jacobwisniewski <jacob.wisniewski.m@gmail.com>
2018-09-24 21:21:50 +1000

    Fixed combineReducers

--- client/src/modules/index.js
+++ client/src/modules/index.js
@@ -1,6 +1,9 @@
-import { combineReducers } from 'redux'
+import { combineReducers } from "redux";
+import todos from './todos'
 // This is where we can import reducers from this folder
 
+// Haven't added a rootReducer yet
 export default combineReducers({
-    // Where we can stick our reducers in to create a root reducer
-})
+    todos
+  // Where we can stick our reducers in to create a root reducer
+});
