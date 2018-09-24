64f1218c3fdc0a3a68a224f7f059f31c01bfc7a9
stevenhvtran <stransteven@outlook.com>
2018-09-24 23:10:30 +1000

    Added callback call to API

--- client/server.js
+++ client/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
+const request = require('request');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/callback', function (req, res) {
   var code = req.query.code || null;
-  return res.send(code);
+  var data = {
+    url: 'http://localhost:5000/api/login',
+    body: {access_code: code},
+    json: true
+  };
+
+  request.post(data, function(error, response, body) {
+    res.send(body)
+  });
 });
 
 app.get('/', function (req, res) {
