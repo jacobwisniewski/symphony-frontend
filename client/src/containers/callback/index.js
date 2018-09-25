d401760cc17f2fe59ce35a79828a2b3b1517ccff
stevenhvtran <stransteven@outlook.com>
2018-09-25 19:22:54 +1000

    Removed redundant code and imports

--- client/src/containers/callback/index.js
+++ client/src/containers/callback/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
 const querystring = require("querystring");
 
 class Callback extends Component {
@@ -13,37 +12,31 @@ class Callback extends Component {
 
   componentDidMount() {
     const values = querystring.parse(this.props.location.search.substring(1));
-    const access_code = values.code
-    this.getAuthUrl(access_code)
+    const access_code = values.code;
+    this.getAuthUrl(access_code);
   }
 
   getAuthUrl(access_code) {
     const url = "http://localhost:5000/api/login";
-    fetch(url,
-        {
-            method: "POST",
-            mode: "cors",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({access_code: access_code})
-        })
+    fetch(url, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ access_code: access_code })
+    })
       .then(response => response.json())
       .then(data =>
         this.props.history.push({
           pathname: "/" + this.props.match.params.page,
-          state:{data: data}
-         }));
+          state: { data: data }
+        })
+      );
   }
 
   render() {
-    if (this.state.isLoaded) {
-      return (
-        <Redirect to={"/" + this.props.match.params.page} data={this.state.data}/>
-      );
-    } else {
-      return <div>Loading...</div>;
-    }
+    return <div>Loading...</div>;
   }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
