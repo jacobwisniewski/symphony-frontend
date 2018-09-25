72e0e00bc731e237ae04d2717886f98a14acaf64
stevenhvtran <stransteven@outlook.com>
2018-09-25 19:32:09 +1000

    Handle protected route /profile

--- client/src/containers/profile/index.js
+++ client/src/containers/profile/index.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
 class Profile extends Component {
   render() {
-    const data = this.props.location.state.data;
-    return (
-      <div>
-        <div>{data.user_name}</div>
-        <div>{data.spotify_id}</div>
-        <img src={data.profile_picture} alt="profile" />
-      </div>
-    );
+    if (this.props.location.state) {
+      const data = this.props.location.state.data;
+      return (
+        <div>
+          <div>{data.user_name}</div>
+          <div>{data.spotify_id}</div>
+          <img src={data.profile_picture} alt="profile" />
+        </div>
+      );
+    } else {
+        // No user credentials
+        return <Redirect to="/" />
+    }
   }
 }
 
