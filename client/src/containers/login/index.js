cf3cb67ef6605b26498c3c44e462c8175d75b7a1
jacobwisniewski <jacob.wisniewski.m@gmail.com>
2018-09-27 22:00:08 +1000

    Changed login flow

--- client/src/containers/login/index.js
+++ client/src/containers/login/index.js
@@ -1,32 +1,57 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux'
-import { getUrl } from '../../modules/loginActions'
+import { connect } from "react-redux";
+import { getUrl } from "../../modules/loginActions";
 
 class Login extends Component {
-  componentDidMount() {
-    this.props.getUrl()
+  constructor() {
+    super();
+    this.onClick = this.onClick.bind(this);
   }
+
+  onClick(event) {
+    const name = event.target.name;
+    this.props.getUrl(name).then(({ url, error }) => {
+      if (error != null) {
+        // Error handling
+        alert("ERROR: Callback invalid");
+      } else {
+        // Change browser url to callback url
+        window.location.href = url;
+      }
+    });
+  }
+
   render() {
-    const { url, loading, error } = this.props.login 
-    if (loading) {
-      return(<p>Loading...</p>)
-    } else {
-      return(<a href={url}>Login</a>)
-    }
+    return (
+      <div>
+        <button name="profile" onClick={this.onClick}>
+          Get profile
+        </button>
+        <button name="create" onClick={this.onClick}>
+          Create gig
+        </button>
+        <button name="join" onClick={this.onClick}>
+          Join gig
+        </button>
+      </div>
+    );
   }
 }
 
-
-
 // Lets the component subscribe to redux state changes
 const mapStateToProps = state => ({
-  ...state
-})
+  url: state.login.url,
+  loading: state.login.loading,
+  error: state.login.error
+});
 
 const mapDispatchToProps = dispatch => ({
   // Add actions to this constant in the format
   // action: () => dispatch(action())
-  getUrl: () => dispatch(getUrl())
-})
+  getUrl: action => dispatch(getUrl(action))
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login);
--- client/src/modules/loginActions.js
+++ client/src/modules/loginActions.js
@@ -1,9 +1,10 @@
-export const getUrl = () => dispatch => {
+export const getUrl = (action) => dispatch => {
   // Start the fetch action
   dispatch({
     type: 'GET_URL_BEGIN'
   });
-  const url = "http://localhost:5000/api/callback";
+  const url = "http://localhost:5000/api/" + action + "/callback";
+  console.log(url)
   fetch(url)
     // Get the response and body from the call
     .then(response => response.json().then(body => ({ response, body })))
@@ -16,6 +17,7 @@ export const getUrl = () => dispatch => {
         });
       } else {
         // The call was a success, push the data to SUCCESS action
+        window.location.href = body.url
         dispatch({
           type: 'GET_URL_SUCCESS',
           payload: body.url
