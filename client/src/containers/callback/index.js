3d001a7509822cf8c62406b3ae47a3cf6ea7d575
jacobwisniewski <jacob.wisniewski.m@gmail.com>
2018-09-29 21:06:56 +1000

    Fixed callback logic

--- client/src/containers/callback/index.js
+++ client/src/containers/callback/index.js
@@ -6,9 +6,9 @@ class Callback extends Component {
   componentDidMount() {
     const { access_code, state } = querystring.parse(this.props.location.search.substring(1)); // Get the values stored in the current url
     // Check if request and response state are the same
-    if (state != this.props.prev_state) {
+    if (state !== this.props.prev_state) {
       alert("ERROR: Request and response identifying codes are not the same");
-      // TODO: Add redirect to '/'
+      this.props.history.push('/')  //Push user back to index
     }
     this.props.access_code = access_code; // Save the access_code to the store
     this.props.history.push("/" + this.props.match.params.page); // Redirect the callback to the specified route
