cc1d4ce51a21fc4ce64d51832abde9345d98ce61
jacobwisniewski <jacob.wisniewski.m@gmail.com>
2018-09-29 21:06:49 +1000

    Updated profile component

--- client/src/containers/profile/index.js
+++ client/src/containers/profile/index.js
@@ -1,33 +1,21 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux'
 
 class Profile extends Component {
-  componentDidMount() {
-      // If not logged in prompt login
-    if (!this.props.location.state) {
-      const url = "http://localhost:5000/api/callback";
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          window.location = data.url
-        });
-    }
-  }
+  componentDidMount() {}
   render() {
-    if (this.props.location.state) {
-      const data = this.props.location.state.data;
-      return (
-        <div>
-          <div>{data.user_name}</div>
-          <div>{data.spotify_id}</div>
-          <a href="/">Logout</a>
-          <img src={data.profile_picture} alt="profile" />
-        </div>
-      );
-    } else {
-      // No user credentials
-      return null
-    }
+    return null;
   }
 }
 
-export default Profile;
+// Lets the component subscribe to redux state changes
+const mapStateToProps = state => ({
+  ...state
+})
+
+const mapDispatchToProps = dispatch => ({
+  // Add actions to this constant in the format
+  // action: () => dispatch(action())
+})
+
+export default connect(mapDispatchToProps, mapStateToProps)(Profile);
