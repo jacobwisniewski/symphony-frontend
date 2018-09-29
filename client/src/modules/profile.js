6f9d1123a73ef26135f78f0a663434ed26fe6222
jacobwisniewski <jacob.wisniewski.m@gmail.com>
2018-09-29 21:10:33 +1000

    Added profile reducer

---
+++ client/src/modules/profile.js
@@ -0,0 +1,15 @@
+initialState = {
+    mongo_id: null,
+    spotify_id: null,
+    user_name: null,
+    profile_picture: null,
+    user_gigs: null
+}
+
+// Profile reducer
+export default (state = initialState, action) => {
+    switch (action.type) {
+      default:
+        return state;
+    }
+  };
