f0ec0785d4183ae4fbc3d7a54a754aba508c3a1b
jacobwisniewski <jacob.wisniewski.m@gmail.com>
2018-10-04 10:40:09 +1000

    Added create forms logic

--- client/src/containers/create/index.js
+++ client/src/containers/create/index.js
@@ -2,8 +2,44 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Create extends Component {
+  constructor() {
+    super();
+    // States for recording input values
+    this.state = {
+      gig_name: "",
+      private: false
+    };
+    // Bind change functions
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    // Function for handling changing inputs
+    const value = event.target.value
+    this.setState({
+        gig_name: value
+    })
+  }
+
+  handleSubmit(event) {
+    // Function for handling submitting values
+    console.log(this.state.gig_name)
+    event.preventDefault();
+
+  }
   render() {
-    return null;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.gig_name}
+          onChange={this.handleChange}
+          placeholder="Gig name"
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    );
   }
 }
 
@@ -15,4 +51,7 @@ const mapDispatchToProps = dispatch => ({
   // action: () => dispatch(action())
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Create);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Create);
