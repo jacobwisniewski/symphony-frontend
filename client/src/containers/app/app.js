db2df43a82a060c03c68cd71253b6a1fab744552
jacobwisniewski <jacob.wisniewski.m@gmail.com>
2018-10-07 23:16:22 +1100

    Fixed syntax and formatting

--- client/src/containers/app/app.js
+++ client/src/containers/app/app.js
@@ -1,36 +1,36 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
-import { connect } from 'react-redux'
-import Login from "../login"
-import Callback from "../callback"
-import Profile from "../profile"
-import Create from "../create"
-
+import { connect } from "react-redux";
+import Login from "../login";
+import Callback from "../callback";
+import Dash from "../dash";
 
 class App extends Component {
-    render() {
-        return(
-            <div>
-                <Route exact path='/' component={Login} />
-                <Route path='/:page/callback' component={Callback} />
-                <Route exact path='/profile' component={Dash} />
-                <Route exact path='/create' component={Dash} />
-                <Route exact path='/join' component={Dash} />
-                <Route exact path='/gigs' component={Dash} />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Route exact path="/" component={Login} />
+        <Route path="/:page/callback" component={Callback} />
+        <Route exact path="/profile" component={Dash} />
+        <Route exact path="/create" component={Dash} />
+        <Route exact path="/join" component={Dash} />
+        <Route exact path="/gigs" component={Dash} />
+      </div>
+    );
+  }
 }
 
 // Lets the component subscribe to redux state changes
 const mapStateToProps = state => ({
-    ...state
-})
+  ...state
+});
 
 const mapDispatchToProps = dispatch => ({
-    // Add actions to this constant in the format
-    // action: () => dispatch(action())
-})
+  // Add actions to this constant in the format
+  // action: () => dispatch(action())
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- client/src/containers/dash/index.js
+++ client/src/containers/dash/index.js
@@ -5,48 +5,57 @@ import {
   toggleCreate,
   toggleGigs
 } from "../../modules/dashActions";
+import Profile from "../profile"
+import Join from "../join"
+import Gigs from "../gigs"
 
 class Dash extends Component {
   constructor() {
-      super()
-      this.onClick = this.onClick.bind(this)
+    super();
+    this.onClick = this.onClick.bind(this);
   }
-componentDidMount() {
-  const currentPath = this.props.location.pathname
-  // Toggle the relevant tab based on the url
-  if (currentPath === "/create") {
-    // Execute toggleCreate
-    this.props.toggleCreate();
-  } else if (currentPath === "/join") {
-    // Execute toggleJoin
-    this.props.toggleJoin();
-  } else if (currentPath === "/gigs") {
-    // Execute toggleGigs
-    this.props.toggleGigs();
-  }
-}
-onClick(event) {
-    if (event.target.name === 'join') {
-      this.props.toggleJoin()
-    } else {
-        this.props.toggleGigs()
+  componentDidMount() {
+    const currentPath = this.props.location.pathname;
+    // Toggle the relevant tab based on the url
+    if (currentPath === "/create") {
+      // Execute toggleCreate
+      this.props.toggleCreate();
+    } else if (currentPath === "/join") {
+      // Execute toggleJoin
+      this.props.toggleJoin();
+    } else if (currentPath === "/gigs") {
+      // Execute toggleGigs
+      this.props.toggleGigs();
     }
-}
-render() {
-  return (
+  }
+  onClick(event) {
+    if (event.target.name === "join") {
+      this.props.toggleJoin();
+      this.props.history.push('/join')
+    } else {
+      this.props.toggleGigs();
+      this.props.history.push('/gigs')
+    }
+  }
+  render() {
+    return (
       <div>
-    <div>
-      <Profile />
-      <button name="join" onClick={this.onClick}>Toggle join</button>
-      { this.props.displayJoin && <Join /> }
+        <div>
+          <Profile />
+          <button name="join" onClick={this.onClick}>
+            Toggle join
+          </button>
+          {this.props.displayJoin && <Join />}
+        </div>
+        <div>
+          <button name="gigs" onClick={this.onClick}>
+            Toggle gigs
+          </button>
+          {this.props.displayGigs && <Gigs />}
+        </div>
       </div>
-      <div>
-      <button name="gigs" onClick={this.onClick}>Toggle gigs</button>
-      { this.props.displayGigs && <Gigs /> }
-    </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 // Lets the component subscribe to redux state changes
