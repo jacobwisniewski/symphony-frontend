1806cc8c23319e530a17c7d51ac1064e1bc4a657
jacobwisniewski <jacob.wisniewski.m@gmail.com>
2018-11-18 18:36:21 +1100

    Updated gigs component

--- client/src/components/gigs.js
+++ client/src/components/gigs.js
@@ -3,20 +3,33 @@ import { connect } from "react-redux";
 
 class Gig extends Component {
   render() {
-    return <div>{this.props.gig_name}</div>;
+    let playlist_url = `https://open.spotify.com/playlist/${
+      this.props.gig_data.playlist_id
+    }`;
+    return (
+      <div>
+        <div>Gig name: {this.props.gig_data.gig_name}</div>
+        <div>Invite code: {this.props.gig_data.invite_code}</div>
+        <div>Gig owner: {this.props.gig_data.owner_name}</div>
+        <a href={playlist_url}>Playlist url</a>
+      </div>
+    );
   }
 }
 
 class Gigs extends Component {
   componentDidMount() {
-    console.log(this.props.user_gigs)
+    console.log(this.props.user_gigs);
   }
   render() {
     const { user_gigs } = this.props;
     return (
       <div>
         {user_gigs.map(gig => (
-          <Gig key={gig.gig_id} gig_name={gig.gig_name} />
+          <div>
+            <Gig key={gig.gig_id} gig_data={gig} />
+            <br />
+          </div>
         ))}
       </div>
     );
