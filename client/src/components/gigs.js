067d3150d2f005285c7ef8173c34507e6b16fcaa
jacobwisniewski <jacob.wisniewski.m@gmail.com>
2018-11-18 12:09:07 +1100

    Created gigs component

--- client/src/components/gigs.js
+++ client/src/components/gigs.js
@@ -1,24 +1,39 @@
-import React, { Component } from "react"
-import { connect } from "react-redux"
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Gig extends Component {
+  render() {
+    return <div>{this.props.gig_name}</div>;
+  }
+}
 
 class Gigs extends Component {
-    render() {
-        return <p>Gigs component</p>
-    }
+  componentDidMount() {
+    console.log(this.props.user_gigs)
+  }
+  render() {
+    const { user_gigs } = this.props;
+    return (
+      <div>
+        {user_gigs.map(gig => (
+          <Gig key={gig.gig_id} gig_name={gig.gig_name} />
+        ))}
+      </div>
+    );
+  }
 }
 
 // Lets the component subscribe to redux state changes
 const mapStateToProps = state => ({
-    ...state
-  });
-  
-  const mapDispatchToProps = dispatch => ({
-    // Add actions to this constant in the format
-    // action: () => dispatch(action())
-  });
-  
-  export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(Gigs);
-  
+  user_gigs: state.profile.user_gigs
+});
+
+const mapDispatchToProps = dispatch => ({
+  // Add actions to this constant in the format
+  // action: () => dispatch(action())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Gigs);
