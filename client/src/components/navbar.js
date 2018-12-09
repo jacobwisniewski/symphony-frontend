f079230cdd14922e7c16dde535e8ea4bcefa97a9
jacobwisniewski <jacob.wisniewski.m@gmail.com>
2018-12-10 10:46:57 +1100

    Commit before CSS styling

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,38 +1,38 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-import { toggleCreate, toggleJoin, toggleGigs } from "../modules/dashActions"
+import { toggleCreate, toggleJoin, toggleGigs } from "../modules/dashActions";
 
 class Button extends Component {
-  render() {
-    return <button onClick={this.props.toggle}>{this.props.name}</button>;
-  }
+	render() {
+		return <button onClick={this.props.toggle}>{this.props.name}</button>;
+	}
 }
 
 class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <Button toggle={this.props.toggleCreate} name={"Create"} />
-        <Button toggle={this.props.toggleJoin} name={"Join"} />
-        <Button toggle={this.props.toggleGigs} name={"Gigs"} />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="navbar-container">
+				<Button toggle={this.props.toggleCreate} name={"Create"} />
+				<Button toggle={this.props.toggleJoin} name={"Join"} />
+				<Button toggle={this.props.toggleGigs} name={"Gigs"} />
+			</div>
+		);
+	}
 }
 
 // Lets the component subscribe to redux state changes
 const mapStateToProps = state => ({});
 
 const mapDispatchToProps = dispatch => ({
-  // Add actions to this constant in the format
-  // action: () => dispatch(action())
-  toggleCreate: () => dispatch(toggleCreate()),
-  toggleJoin: () => dispatch(toggleJoin()),
-  toggleGigs: () => dispatch(toggleGigs())
+	// Add actions to this constant in the format
+	// action: () => dispatch(action())
+	toggleCreate: () => dispatch(toggleCreate()),
+	toggleJoin: () => dispatch(toggleJoin()),
+	toggleGigs: () => dispatch(toggleGigs()),
 });
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps,
 )(withRouter(Navbar));
