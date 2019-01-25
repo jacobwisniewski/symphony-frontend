0d7edfe3068d12c8e7c105b0a83c9f2e4674b17c
jacobwisniewski <jacob.wisniewski.m@gmail.com>
2019-01-25 12:02:08 +1100

    Fixed getUrl action creator

--- client/src/modules/loginActions.js
+++ client/src/modules/loginActions.js
@@ -1,32 +1,37 @@
 import { api_url } from "../index";
 
 export const getUrl = () => dispatch => {
-  // Fetches the Spotify authorisation URL from the backend
-  dispatch({
-    type: "URL_FETCH_BEGIN"
-  });
+	// Fetches the Spotify authorisation URL from the backend
+	dispatch({
+		type: "URL_FETCH_BEGIN",
+	});
 
-  const url = api_url + "/callback";
-  return fetch(url)
-    .then(response => response.json().then(body => ({ response, body })))
-    .then(({ response, body }) => {
-      if (!response.ok) {
-        dispatch({
-          type: "URL_FETCH_FAILURE",
-          payload: { error: response.statusText }
-        });
-      } else {
-        dispatch({
-          type: "URL_FETCH_SUCCESS",
-          payload: body
-        });
-        return body.url;
-      }
-    });
+	const url = api_url + "/callback";
+	return fetch(url, {
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+	})
+		.then(response => response.json().then(body => ({ response, body })))
+		.then(({ response, body }) => {
+			if (!response.ok) {
+				dispatch({
+					type: "URL_FETCH_FAILURE",
+					payload: { error: response.statusText },
+				});
+			} else {
+				dispatch({
+					type: "URL_FETCH_SUCCESS",
+					payload: body,
+				});
+				return body.url;
+			}
+		});
 };
 
 export const userLogout = () => dispatch => {
-  dispatch({
-    type: 'USER_LOGOUT'
-  })
-}
+	dispatch({
+		type: "USER_LOGOUT",
+	});
+};
